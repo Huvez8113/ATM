fe2902de16dd28a7efc60b1dd86f18b42751ead7
P.K.Huvez Ahmed <113931978+Huvez8113@users.noreply.github.com>
2023-12-07 21:18:29 +0530

    Add files via upload

---
+++ src/atm_project/ATM.java
@@ -0,0 +1,14 @@
+package atm_project;
+
+import java.io.IOException;
+
+
+public class ATM {
+
+		public static void main(String[] args) throws IOException{
+			OptionsMenu Menu=new OptionsMenu();
+			Menu.getLogin();
+	
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author H.A.M
+ *
+ */
+module atm_project {
+}
